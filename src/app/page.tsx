e5e2ee7bbe306fb0dfe56fc16c8fab7ed06870c3
terasdigital <terasdigital24@gmail.com>
2026-04-14 13:14:53 +0700

    feat:setup sidebar-menu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { DarkmodeToggle } from "@/components/common/darkmode-toggle";
 import { Button } from "@/components/ui/button";
-
+import Link from "next/link";
 export default function Home() {
   return (
-    <div>
-      <Button>Hello</Button>
-      <DarkmodeToggle></DarkmodeToggle>
+    <div className="bg-muted flex flex-col h-screen justify-center items-center space-y-4">
+      <h1 className="text-4xl font-semibold">Welcome Adhitya Ramadhan Putra</h1>
+      <Link href="/admin">
+        <Button className="bg-teal-500 text-white">Access to Dashboard</Button>
+      </Link>
     </div>
   );
 }
--- src/components/common/app-sidebar.tsx
+++ src/components/common/app-sidebar.tsx
@@ -5,6 +5,8 @@ import {
   Sidebar,
   SidebarContent,
   SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
   SidebarHeader,
   SidebarMenu,
   SidebarMenuButton,
@@ -21,54 +23,74 @@ import {
   DropdownMenuTrigger,
 } from "../ui/dropdown-menu";
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import {
+  SIDEBAR_MENU_LIST,
+  SidebarMenuKey,
+} from "@/constants/sidebar-constant";
+import { TooltipProvider } from "../ui/tooltip";
+import { cn } from "@/lib/utils";
+import { usePathname } from "next/navigation";
 
 export default function AppSidebar() {
   const { isMobile } = useSidebar();
+  const pathname = usePathname();
+  const profile = {
+    name: "Adhitya Ramadhan Putra",
+    role: "admin",
+    avatar_url: "",
+  };
   return (
-    <Sidebar>
-      <SidebarHeader>
-        <SidebarMenu>
-          <SidebarMenuItem>
-            <SidebarMenuButton asChild>
-              <div className="font-semibold">
-                <div className="bg-teal-400 flex items-center justify-center p-2 rounded">
-                  <TreePalm />
-                </div>
-                UmrahIn
-              </div>
-            </SidebarMenuButton>
-          </SidebarMenuItem>
-        </SidebarMenu>
-      </SidebarHeader>
-      <SidebarFooter>
-        <SidebarMenu>
-          <SidebarMenuItem>
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <SidebarMenuButton size="lg">
-                  <Avatar>
-                    <AvatarImage src="" alt="" />
-                    <AvatarFallback className="rounded-lg">A</AvatarFallback>
-                  </Avatar>
-                  <div className="leading-tight">
-                    <h4 className="truncate font-medium">
-                      Adhitya Ramadhan Putra
-                    </h4>
-                    <p className="text-muted-foreground truncate text-xs">
-                      Admin
-                    </p>
+    <TooltipProvider>
+      <Sidebar collapsible="icon">
+        <SidebarHeader>
+          <SidebarMenu>
+            <SidebarMenuItem>
+              <SidebarMenuButton asChild>
+                <div className="font-semibold">
+                  <div className="bg-teal-400 flex items-center justify-center p-2 rounded">
+                    <TreePalm />
                   </div>
-                  <EllipsisVertical className="ml-auto size-4" />
-                </SidebarMenuButton>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent
-                className="min-w-56 rounded-lg"
-                side={isMobile ? "bottom" : "right"}
-                align="end"
-                sideOffset={4}
-              >
-                <DropdownMenuLabel>
-                  <div className="flex items-center gap-2 px-1 py-1.5">
+                  UmrahIn
+                </div>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          </SidebarMenu>
+        </SidebarHeader>
+        <SidebarContent>
+          <SidebarGroup>
+            <SidebarGroupContent className="flex flex-col gap-2">
+              <SidebarMenu>
+                {SIDEBAR_MENU_LIST[profile.role as SidebarMenuKey]?.map(
+                  (item) => (
+                    <SidebarMenuItem key={item.title}>
+                      <SidebarMenuButton asChild tooltip={item.title}>
+                        <a
+                          href={item.url}
+                          className={cn("px-4 py-3 h-auto", {
+                            "bg-teal-500 text-white hover:bg-teal-500 hover:text-white":
+                              pathname === item.url,
+                          })}
+                        >
+                          {item.icon && <item.icon />}
+                          <span>{item.title}</span>
+                        </a>
+                      </SidebarMenuButton>
+                    </SidebarMenuItem>
+                  ),
+                )}
+              </SidebarMenu>
+            </SidebarGroupContent>
+          </SidebarGroup>
+        </SidebarContent>
+        <SidebarFooter>
+          <SidebarMenu>
+            <SidebarMenuItem>
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <SidebarMenuButton
+                    size="lg"
+                    className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+                  >
                     <Avatar>
                       <AvatarImage src="" alt="" />
                       <AvatarFallback className="rounded-lg">A</AvatarFallback>
@@ -81,20 +103,46 @@ export default function AppSidebar() {
                         Admin
                       </p>
                     </div>
-                  </div>
-                </DropdownMenuLabel>
-                <DropdownMenuSeparator />
-                <DropdownMenuGroup>
-                  <DropdownMenuItem>
-                    <LogOut />
-                    Logout
-                  </DropdownMenuItem>
-                </DropdownMenuGroup>
-              </DropdownMenuContent>
-            </DropdownMenu>
-          </SidebarMenuItem>
-        </SidebarMenu>
-      </SidebarFooter>
-    </Sidebar>
+                    <EllipsisVertical className="ml-auto size-4" />
+                  </SidebarMenuButton>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent
+                  className="min-w-56 rounded-lg"
+                  side={isMobile ? "bottom" : "right"}
+                  align="end"
+                  sideOffset={4}
+                >
+                  <DropdownMenuLabel>
+                    <div className="flex items-center gap-2 px-1 py-1.5">
+                      <Avatar>
+                        <AvatarImage src="" alt="" />
+                        <AvatarFallback className="rounded-lg">
+                          A
+                        </AvatarFallback>
+                      </Avatar>
+                      <div className="leading-tight">
+                        <h4 className="truncate font-medium">
+                          Adhitya Ramadhan Putra
+                        </h4>
+                        <p className="text-muted-foreground truncate text-xs">
+                          Admin
+                        </p>
+                      </div>
+                    </div>
+                  </DropdownMenuLabel>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuGroup>
+                    <DropdownMenuItem>
+                      <LogOut />
+                      Logout
+                    </DropdownMenuItem>
+                  </DropdownMenuGroup>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </SidebarMenuItem>
+          </SidebarMenu>
+        </SidebarFooter>
+      </Sidebar>
+    </TooltipProvider>
   );
 }
