931b8d06dff40aa0256b3c4db5491173698019a4
terasdigital <terasdigital24@gmail.com>
2026-04-29 18:55:19 +0700

    feat:create 'create menu'

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {},
+  devIndicators: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "rzgkrctocxqbmpusilxl.supabase.co",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
---
+++ src/app/(dashboard)/admin/menu/page.tsx
@@ -0,0 +1,9 @@
+import MenuManagement from "./_components/menu";
+
+export const metadata = {
+  title: "Umrahin | Menu Management",
+};
+
+export default function MenuManagementPage() {
+  return <MenuManagement />;
+}
