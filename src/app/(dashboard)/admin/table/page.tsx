431f8170d075f7ead79299eae7f85decf8a74b96
terasdigital <terasdigital24@gmail.com>
2026-05-08 15:59:11 +0700

    feat:table schema

---
+++ src/app/(dashboard)/admin/table/page.tsx
@@ -0,0 +1,9 @@
+import TableManagement from "./_components/table";
+
+export const metadata = {
+  title: "Umrahin | Table Management",
+};
+
+export default function TableManagementPage() {
+  return <TableManagement />;
+}
