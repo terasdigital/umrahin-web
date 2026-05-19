cb026569b2299fbb708d8c278c98feb03e3f3a0e
terasdigital <terasdigital24@gmail.com>
2026-05-19 21:42:58 +0700

    feat:order list

---
+++ src/app/(dashboard)/order/page.tsx
@@ -0,0 +1,9 @@
+import OrderManagement from "./_components/order";
+
+export const metadata = {
+  title: "Umrahin | Order Management",
+};
+
+export default function OrderManagementPage() {
+  return <OrderManagement />;
+}
