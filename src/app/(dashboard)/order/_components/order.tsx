29ae784775635619e6142ae980a9b3c362c7776c
terasdigital <terasdigital24@gmail.com>
2026-05-22 08:11:52 +0700

    feat:handle reservation

--- src/app/(dashboard)/order/_components/order.tsx
+++ src/app/(dashboard)/order/_components/order.tsx
@@ -7,12 +7,25 @@ import { Input } from "@/components/ui/input";
 import useDataTable from "@/hooks/use-data-table";
 import { createClient } from "@/lib/supabase/client";
 import { useQuery } from "@tanstack/react-query";
-import { useMemo, useState } from "react";
+import {
+  startTransition,
+  useActionState,
+  useEffect,
+  useMemo,
+  useState,
+} from "react";
 import { toast } from "sonner";
 import { cn } from "@/lib/utils";
 import { Table } from "@/validations/table-validation";
-import { HEADER_TABLE_ORDER } from "@/constants/order-constant";
+import {
+  HEADER_TABLE_ORDER,
+  INITIAL_STATE_ORDER,
+} from "@/constants/order-constant";
 import DialogCreateOrder from "./dialog-create-order";
+import { updateReservation } from "../actions";
+import { INITIAL_STATE_ACTION } from "@/constants/general-constant";
+import { Ban, Link2Icon, ScrollText } from "lucide-react";
+import Link from "next/link";
 
 export default function OrderManagement() {
   const supabase = createClient();
@@ -81,6 +94,76 @@ export default function OrderManagement() {
     if (!open) setSelectedAction(null);
   };
 
+  const totalPages = useMemo(() => {
+    return orders && orders.count !== null
+      ? Math.ceil(orders.count / currentLimit)
+      : 0;
+  }, [orders]);
+
+  const [reservedState, reservedAction] = useActionState(
+    updateReservation,
+    INITIAL_STATE_ACTION,
+  );
+
+  const handelReservation = async ({
+    id,
+    table_id,
+    status,
+  }: {
+    id: string;
+    table_id: string;
+    status: string;
+  }) => {
+    const formData = new FormData();
+    Object.entries({ id, table_id, status }).forEach(([Key, value]) => {
+      formData.append(Key, value);
+    });
+    startTransition(() => {
+      reservedAction(formData);
+    });
+  };
+  useEffect(() => {
+    if (reservedState?.status === "error") {
+      toast.error("Update Reservation Failed", {
+        description: reservedState.errors?._form?.[0],
+      });
+      // startTransition(() => {
+      //   createMenuAction(null);
+      // });
+    }
+
+    if (reservedState?.status === "success") {
+      toast.success("Update Reservation Success");
+      refetch();
+      refetchTables();
+    }
+  }, [reservedState]);
+
+  const reservedActionList = [
+    {
+      label: (
+        <span className="flex items-center gap-2">
+          <Link2Icon />
+          Process
+        </span>
+      ),
+      action: (id: string, table_id: string) => {
+        handelReservation({ id, table_id, status: "process" });
+      },
+    },
+    {
+      label: (
+        <span className="flex items-center gap-2">
+          <Ban className="text-red-500" />
+          Cancel
+        </span>
+      ),
+      action: (id: string, table_id: string) => {
+        handelReservation({ id, table_id, status: "canceled" });
+      },
+    },
+  ];
+
   const filteredData = useMemo(() => {
     return (orders?.data || []).map((order, index) => {
       return [
@@ -99,15 +182,35 @@ export default function OrderManagement() {
         >
           {order.status}
         </div>,
-        <DropdownAction menu={[]} />,
+        <DropdownAction
+          menu={
+            order.status === "reserved"
+              ? reservedActionList.map((item) => ({
+                  label: item.label,
+                  action: () =>
+                    item.action(
+                      order.id,
+                      (order.tables as unknown as { id: string }).id,
+                    ),
+                }))
+              : [
+                  {
+                    label: (
+                      <Link
+                        href={`/order/${order.order_id}`}
+                        className="flex items-center gap-2"
+                      >
+                        <ScrollText />
+                        Detail
+                      </Link>
+                    ),
+                    type: "link",
+                  },
+                ]
+          }
+        />,
       ];
     });
-  }, [orders]);
-
-  const totalPages = useMemo(() => {
-    return orders && orders.count !== null
-      ? Math.ceil(orders.count / currentLimit)
-      : 0;
   }, [orders]);
 
   return (
--- src/types/general.d.ts
+++ src/types/general.d.ts
@@ -1,4 +1,4 @@
-export type formState = {
+export type FormState = {
   status?: string;
   errors?: {
     _form?: string[];
